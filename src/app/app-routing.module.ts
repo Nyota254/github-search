84385a35e020b193bf2cec6c172cf89beb28d7bd
Nyota254 <knyota66@gmail.com>
2019-08-23 14:03:12 +0300

    add the nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubSearchComponent } from './github-search/github-search.component';
+import { GithubSearchformComponent } from './github-searchform/github-searchform.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', 'component':GithubSearchComponent},
+  { path:'GithubSearchformComonent', 'component':GithubSearchformComponent},
+  { path:'', redirectTo:"/home", pathMatch:"full"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
 import { GithubSearchformComponent } from './github-searchform/github-searchform.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubSearchComponent,
-    GithubSearchformComponent
+    GithubSearchformComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
