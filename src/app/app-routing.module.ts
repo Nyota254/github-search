538921624abea6efba4aac167e73485280f7968a
Nyota254 <knyota66@gmail.com>
2019-08-24 08:58:32 +0300

    add the routing to the repositorys search page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GithubSearchComponent } from './github-search/github-search.component';
-import { GithubSearchformComponent } from './github-searchform/github-searchform.component';
+import { RepositorySearchpageComponent} from './repository-searchpage/repository-searchpage.component';
 
 
 const routes: Routes = [
   { path:'home', 'component':GithubSearchComponent},
-  { path:'GithubSearchformComonent', 'component':GithubSearchformComponent},
+  {path:'repositorys','component':RepositorySearchpageComponent},
   { path:'', redirectTo:"/home", pathMatch:"full"},
 ];
 
