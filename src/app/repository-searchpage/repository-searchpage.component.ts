b716372bb5baebb9aa788e0b761b328035088bf6
Nyota254 <knyota66@gmail.com>
2019-08-25 11:41:01 +0300

    add functionality to the form and also fetcth data from api

--- src/app/repository-searchpage/repository-searchpage.component.ts
+++ src/app/repository-searchpage/repository-searchpage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubHttpService } from '../github-http.service';
+import { Repositorys } from '../repositorys';
 
 @Component({
   selector: 'app-repository-searchpage',
@@ -7,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositorySearchpageComponent implements OnInit {
 
-  constructor() { }
+  repository:Repositorys[];
+
+  constructor(private _http:GithubHttpService) { }
+
+  repoName(RepoName:string){
+    this._http.searchRepositorys(RepoName,10).then(
+      () => {
+        this.repository = this._http.repositorys;
+        console.log(RepoName);
+        console.log(this.repository);
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  }
 
   ngOnInit() {
   }
